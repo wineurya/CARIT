6e8fb5a946f17541ad7f637a44767204fbbcd7ca
wineurya <124418212+wineurya@users.noreply.github.com>
2023-04-25 03:11:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 // get form elements
 const studentStatusSelect = document.getElementById("student-status");
-const coursesContainer = document.getElementById("courses-container");
 const evaluateBtn = document.getElementById("evaluate-btn");
 const resultContainer = document.getElementById("result-container");
-
-// define courses for different student statuses
-const courses = {
-  undergraduate: ["Course 1", "Course 2", "Course 3"],
-  graduate: ["Course 4", "Course 5", "Course 6"],
-};
 
 // define point grades for letter grades
 const pointGrades = {
@@ -19,35 +12,24 @@ const pointGrades = {
   F: 0.0,
 };
 
-// create course grade inputs
-function createCourseInputs(courses) {
-  let html = "";
-  for (let i = 0; i < courses.length; i++) {
-    html += `<div class="form-group">
-              <label for="course${i + 1}">${courses[i]}</label>
-              <select id="course${i + 1}" name="course${i + 1}" class="form-control">
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="F">F</option>
-              </select>
-            </div>`;
-  }
-  return html;
-}
-
-// display course grade inputs based on student status
+// toggle course display based on student status
 function displayCourseInputs() {
   const studentStatus = studentStatusSelect.value;
-  const studentCourses = courses[studentStatus];
-  const courseInputsHtml = createCourseInputs(studentCourses);
-  coursesContainer.innerHTML = courseInputsHtml;
+  const undergraduateCourses = document.querySelectorAll(".undergraduate-course");
+  const graduateCourses = document.querySelectorAll(".graduate-course");
+
+  if (studentStatus === "undergraduate") {
+    undergraduateCourses.forEach(course => course.style.display = "block");
+    graduateCourses.forEach(course => course.style.display = "none");
+  } else {
+    undergraduateCourses.forEach(course => course.style.display = "none");
+    graduateCourses.forEach(course => course.style.display = "block");
+  }
 }
 
 // calculate average grade and display result
 function evaluateGrades() {
-  const courseSelects = coursesContainer.querySelectorAll("select");
+  const courseSelects = document.querySelectorAll("#courses-container select");
   let totalPoints = 0;
   for (let i = 0; i < courseSelects.length; i++) {
     const letterGrade = courseSelects[i].value;
@@ -55,7 +37,7 @@ function evaluateGrades() {
     totalPoints += pointGrade;
   }
   const averageGrade = totalPoints / courseSelects.length;
-  const message = `Your average grade is ${averageGrade.toFixed(2)}.`;
+  let message = `Your average grade is ${averageGrade.toFixed(2)}.`;
   if (averageGrade >= 3.0) {
     message += " Congratulations, you meet the average grade requirement.";
   } else {
@@ -67,3 +49,6 @@ function evaluateGrades() {
 // add event listeners
 studentStatusSelect.addEventListener("change", displayCourseInputs);
 evaluateBtn.addEventListener("click", evaluateGrades);
+
+// display course inputs initially
+displayCourseInputs();
