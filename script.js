4d407ff53fb40013ecd451e01a00f95f713d1c94
wineurya <124418212+wineurya@users.noreply.github.com>
2023-04-25 21:20:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 window.onload = function() {
-  // get form elements
   const studentStatusSelect = document.getElementById("student-status");
   const evaluateBtn = document.getElementById("evaluate-btn");
   const resultContainer = document.getElementById("result-container");
 
-  // define point grades for letter grades
-  const pointGrades = {
-    A: 4.0,
-    B: 3.0,
-    C: 2.0,
-    D: 1.0,
-    F: 0.0,
+  const pointGrades = { 
+    A: 4.0, B: 3.0,C: 2.0,D: 1.0,F: 0.0,
   };
 
- // toggle course display based on student status
 function displayCourseInputs() {
   const studentStatus = studentStatusSelect.value;
   const undergraduateCourses = document.querySelectorAll(".undergraduate-course");
@@ -32,7 +25,6 @@ function displayCourseInputs() {
 }
 
 
-  // calculate average grade and display result
   function evaluateGrades() {
     const courseSelects = document.querySelectorAll("#courses-container select");
     let displayedCourses = 0;
@@ -61,10 +53,8 @@ function displayCourseInputs() {
     }
   }
   
-  // add event listeners
   studentStatusSelect.addEventListener("change", displayCourseInputs);
   evaluateBtn.addEventListener("click", evaluateGrades);
 
-  // display course inputs initially
   displayCourseInputs();
 }
